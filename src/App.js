d6faf16fca5be4939ebbfe2bd049eb9f15f7e262
dailxx <zhangzhiheng456@gmail.com>
2018-07-30 23:43:43 +0800

    '11'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo      from './logo.svg';
+import      './App.css';
 
 class App extends Component {
+  state = {
+
+  }
   render() {
+    let dict = {key: 'value'};
     return (
       <div className="App">
-        <header className="App-header">
+        <header     className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
